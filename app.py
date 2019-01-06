dd2569d77745c2613d331d8b9841eba2e59be477
shixuezhe <1159513993@qq.com>
2019-01-06 14:42:14 +0800

    geng

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class File(db.Model):
         self.category=category
         self.content=content
     def __repr__(self):
-        return '%r' % self.title
+        return '%s' % self.title
 
 class Category(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -34,26 +34,12 @@ class Category(db.Model):
     def __repr__(self):
         return '%r' % self.name
 
-
-#@app.route('/')
-#def index():
-#    title_list=os.listdir('/home/shiyanlou/files/')
-#    file_name=[]
-#    for i in title_list:
-#        a=i.split('.')
-#        file_name.append(a[0])
-#    return render_template('index.html',file_name=file_name)
+title_list=File.query.all()
+@app.route('/')
+def index():
+    return render_template('index.html',title_list=title_list)
 
 #@app.route('/files/<filename>')
 #def file(filename):
-#    if os.path.exists('/home/shiyanlou/files/'+ filename +'.json')
-#        with open('/home/shiyanlou/files/'+filename+'.json','r') as f:
-#            file_content=json.load(f)
-#            file_print=file_content['content']
-#        return render_template('file.html',file_print=file_print)
-#    else:
-#        return render_template('404.html'),404
-#@app any('/<anything>')
-#def any(anything):
-#    return render_template('404.html'),404
+#    return render_template('file.html',file_print=file_print)
 
