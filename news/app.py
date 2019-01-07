2c4665b0f108c44db96d8c1716cfad0f20628a34
shixuezhe <1159513993@qq.com>
2019-01-08 01:12:58 +0800

    gengxin

--- news/app.py
+++ news/app.py
@@ -6,10 +6,13 @@ from datetime import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from pymongo import MongoClient
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/shiyanlou'
 db=SQLAlchemy(app)
+client=MongoClient('127.0.0.1',27017)
+da=client.shiyanlou
 
 class File(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -23,9 +26,28 @@ class File(db.Model):
         self.created_time=datetime.utcnow()
         self.category=category
         self.content=content
+    def add_tag(self,tag_name):
+        self.tag_name=tag_name
+        mon_name=self.title
+        da.mon_name.insert_one({mon_name:tag_name})
+        label_dict=da.mon_name.find()
+        return label_dict
+    def remove_tag(self,tag_name):
+        self.tag_name=tag_name
+        mon_name=self.title
+        da.mon_name.delete_one({mon_name:tag_name})
     def __repr__(self):
         return '%s' % self.title
-
+    file_title=self.title
+    return file_title
+@property
+def tags(self):
+    tag_list=da.file_title.find()
+    tag_content=[]
+    for tag in tag_list:
+        tag_content.append(tag_list[file_title])
+        return tag_content
+    
 class Category(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(80))
@@ -35,9 +57,10 @@ class Category(db.Model):
         return '%r' % self.name
 
 title_list=File.query.all()
+tags()
 @app.route('/')
 def index():
-    return render_template('index.html',title_list=title_list)
+    return render_template('index.html',title_list=title_list,tag_content=tag_content)
 
 @app.route('/files/<file_id>')
 def file(file_id):
