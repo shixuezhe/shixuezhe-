20d3022dc0e1c009c9d3ca8b758d91ee4f3fd134
shixuezhe <1159513993@qq.com>
2019-02-17 16:19:04 +0800

    第七周挑战三

--- seventh_week-3/challenge7_3.py
+++ seventh_week-3/challenge7_3.py
@@ -1,55 +1,55 @@
-#_*_ coding:utf-8 _*_
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 def climate_plot():
-    df_temperature = pd.read_excel("GlobalTemperature.xlsx")
-
-    df_climate = pd.read_excel("ClimateChange.xlsx", sheetname='Data')
-
-    data=df_climate[df_climate['Series code'].isin(['EN.ATM.CO2E.KT','EN.ATM.METH.KT.CE','EN.ATM.NOXE.KT.CE','EN.ATM.GHGO.KT.CE','EN.CLC.GHGR.MT.CE'])]     
-    air_data=data.drop(data.columns[:6],axis=1)
-    air_data=air_data.drop([2011],axis=1)
-    air_data.replace({'..':pd.np.nan})
-    air_data=air_data.fillna(method='ffill',axis=1).fillna(method='bfill',axis=1)
-    air_sum=air_data.sum()
-#    air_sum=air_sum.drop([2011])
-
-    data1=df_temperature.drop(['Land Max Temperature','Land Min Temperature'],axis=1)
-    i=pd.to_datetime(data1['Date'])
-    data_1=pd.DataFrame(data=data1.values,columns=data1.columns,index=i)
-    data_1=data_1.dropna(thresh=2)
-    data_1=data_1.fillna(value=0)
-    data_2=data_1.resample('A').mean()
-    data_2=data_2['1990-12-31':'2010-12-31']
-    data_2.index=air_sum.index
+    #提取数据
+    df_climate = pd.read_excel("ClimateChange.xlsx")
+    l = ['EN.ATM.CO2E.KT','EN.ATM.METH.KT.CE','EN.ATM.NOXE.KT.CE',
+         'EN.ATM.GHGO.KT.CE','EN.CLC.GHGR.MT.CE']
+    #用isin的方法提取数据，用索引提取需要的列数据
+    data = df_climate[df_climate['Series code'].isin(l)].iloc[:, 6:-1]
+    #inplace参数是对原数据进行修改的意思
+    data.replace({'..': pd.np.nan}, inplace=True)
+    data = data.fillna(method='ffill', axis=1).fillna(method='bfill', axis=1).sum()
+    #求和生成的是Series数据，需要转换为DataFrame数据
+    data = pd.DataFrame(data.values, index=data.index, columns=['Total GHG'])
     
-    data_3=data_1.resample('M').mean()
-    data_sum=pd.concat([air_sum,data_2],axis=1)
-    data_sum.rename(columns={0:'Total GHG'})
-    data_sum=(data_sum-data_sum.min())/(data_sum.max()-data_sum.min())
+    #提取温度数据
+    gt = pd.read_excel("GlobalTemperature.xlsx")
+    #设置日期为索引，提取两行数据，必须是日期才能用resample进行重采样
+    gt = gt.iloc[:, [1, 4]].set_index(pd.to_datetime(gt.Date))
+    gt.fillna(0, inplace=True)
+    gt_year = gt.resample('a').mean()['1990': '2010']
+    gt_qua = gt.resample('q').mean()
+    df = pd.concat([gt_year.set_index(data.index), data], axis=1)
+    #归一化数据
+    df = df.apply(lambda x: (x-x.min())/(x.max()-x.min()))
 
+    #折线图
     fig=plt.subplot(2,2,1)
-    plt.xlabe('Years')
+    plt.xlabel('Years')
     plt.ylabel('Values')
-    data_sum.plot(kind='line',ax=fig)
-
+    df.plot(kind='line',ax=fig)
+    
+    #柱状图
     fig=plt.subplot(2,2,2)
     plt.xlabel('Years')
     plt.ylabel('Values')
-    data_sum.plot(kind='bar',ax=fig)
+    df.plot(kind='bar',ax=fig)
 
+    #面积图
     fig=plt.subplot(2,2,3)
     plt.xlabel('Quarters')
     plt.ylabel('Temperature')
-    data_3.plot(kind='area',ax=fig)
-
+    gt_qua.plot(kind='area',ax=fig)
+    
+    #密度图
     fig=plt.subplot(2,2,4)
     plt.xlabel('Values')
     plt.xlabel('Values')
-    data_3.plot(kind='kde',ax=fig)
-
+    gt_qua.plot(kind='kde',ax=fig)
+    
+    #自动调整分布
     plt.tight_layout()
     plt.show()
 
